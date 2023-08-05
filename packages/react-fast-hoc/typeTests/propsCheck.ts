f46bc3bd3cad0bc0422d27fac24155cd083864f1
XantreGodlike <valerik007.vs@gmail.com>
2023-08-05 21:06:30 +0300

    added more tests

--- packages/react-fast-hoc/typeTests/propsCheck.ts
+++ packages/react-fast-hoc/typeTests/propsCheck.ts
@@ -10,12 +10,24 @@ const addBebeHoc = createTransformProps<[Objects.Omit<"bebe">]>((props) => ({
   bebe: true as const,
 }));
 
+const addBebeHoc2 = createTransformProps<{
+  type: "props";
+  fn: Objects.Omit<"bebe">;
+}>((props) => ({
+  ...props,
+  bebe: true as const,
+}));
+
 const cssInJsHoc = createTransformProps((props) => ({
   ...props,
   className: [props.className, "bebe"].join(" ").trim(),
 }));
 
+const ComponentThatActuallyHasBebeProp = (props: { bebe: boolean }) => null;
+
 const ComponentWithBebe = addBebeHoc(Component);
+const ComponentWithBebe2 = addBebeHoc2(Component);
+const ComponentWithBebeActual = addBebeHoc2(ComponentThatActuallyHasBebeProp);
 const ComponentWithCssInJs = cssInJsHoc(Component);
 const ComponentWithBebeAndCssInJs = cssInJsHoc(ComponentWithBebe);
 declare class A extends React.Component<{ a: number }> {}
@@ -27,6 +39,12 @@ checks([
     ComponentPropsWithRef<typeof Component>,
     Pass
   >(),
+  check<
+    ComponentPropsWithRef<typeof ComponentWithBebe2>,
+    ComponentPropsWithRef<typeof Component>,
+    Pass
+  >(),
+  check<ComponentPropsWithRef<typeof ComponentWithBebeActual>, {}, Pass>(),
   check<
     ComponentPropsWithRef<typeof ComponentWithCssInJs>,
     ComponentPropsWithRef<typeof Component>,
